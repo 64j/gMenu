1ba08a0320daf0758ddcfb3df6967cc396af546d
Serg <64j@mail.ru>
2016-11-11 23:37:40 +0300

    Update snippet.gmenu.php

--- snippet.gmenu.php
+++ snippet.gmenu.php
@@ -1,27 +1,35 @@
 <?php
-if (!defined('MODX_BASE_PATH'))
-  die();
+if(!defined('MODX_BASE_PATH')) {
+	die();
+}
 
-$gmenu_base = $modx->config['base_path']."assets/snippets/gmenu/";
+$gmenu_base = $modx->config['base_path'] . "assets/snippets/gmenu/";
 $userCfg = (isset($config)) ? "{$gmenu_base}configs/{$config}.config.php" : "{$gmenu_base}configs/default.config.php";
 include_once("class.gmenu.php");
 
 $gm = new gMenu();
-if (file_exists($userCfg)) {
-	include_once("$userCfg");
-}
 $gm->userCfg = isset($config) ? true : false;
-$gm->_cfg    = array(
-  'id' => isset($startId) ? $startId : $modx->documentIdentifier,
-  'ph' => isset($ph) ? $ph : false,
+
+if(file_exists($userCfg)) {
+	$config = array();
+	include_once("$userCfg");
+	$modx->event->params = array_merge($modx->event->params, $config);
+}
+
+$gm->_cfg = array(
+	'id' => isset($startId) ? $startId : $modx->documentIdentifier,
+	'ph' => isset($ph) ? $ph : false,
 	'rowIdPrefix' => isset($rowIdPrefix) ? $rowIdPrefix : false,
-  'hideSubMenus' => isset($hideSubMenus) ? $hideSubMenus : false,
-  'hideFirstLevel' => isset($hideSubMenus) ? $hideSubMenus : false,
-  'limit' => isset($limit) ? $limit : 0,
-	'staticKeys' => isset($staticKeys) ? true : false
+	'hideSubMenus' => isset($hideSubMenus) ? $hideSubMenus : false,
+	'hideFirstLevel' => isset($hideSubMenus) ? $hideSubMenus : false,
+	'limit' => isset($limit) ? $limit : 0,
+	'staticKeys' => isset($staticKeys) && $staticKeys == 1 ? true : false,
+	'cachefile' => isset($cachefile) ? $cachefile : MODX_BASE_PATH . 'assets/cache/gmenu3.pageCache.php',
+	'level' => isset($level) ? $level : '',
+	'tvList' => isset($tvList) ? $tvList : ''
 );
 
-$gm->_tpl    = array(
+$gm->_tpl = array(
 	'outerTpl' => isset($outerTpl) ? $outerTpl : '',
 	'rowTpl' => isset($rowTpl) ? $rowTpl : '',
 	'parentRowTpl' => isset($parentRowTpl) ? $parentRowTpl : '',
@@ -32,7 +40,7 @@ $gm->_tpl    = array(
 	'innerHereTpl' => isset($innerHereTpl) ? $innerHereTpl : '',
 	'activeParentRowTpl' => isset($activeParentRowTpl) ? $activeParentRowTpl : '',
 	'categoryFoldersTpl' => isset($categoryFoldersTpl) ? $categoryFoldersTpl : '',
-	'startItemTpl' => isset($startItemTpl) ? $startItemTpl : '',
+	'startItemTpl' => isset($startItemTpl) ? $startItemTpl : ''
 );
 
 $gm->_css = array(
@@ -41,12 +49,17 @@ $gm->_css = array(
 	'here' => isset($hereClass) ? $hereClass : 'active',
 	'parent' => isset($parentClass) ? $parentClass : 'parent',
 	'row' => isset($rowClass) ? $rowClass : '',
-	'level' => isset($levelClass) ? $levelClass: '',
+	'level' => isset($levelClass) ? $levelClass : '',
 	'outer' => isset($outerClass) ? $outerClass : '',
-	'inner' => isset($innerClass) ? $innerClass : '',
-	'level' => isset($levelClass) ? $levelClass: '',
+	'inner' => isset($innerClass) ? $innerClass : ''
 );
 
-$output      = $gm->run();
+foreach ($modx->event->params as $key => $value) {
+	if (substr($key, -3, 3) == 'Tpl') {
+		$gm->_tpl[$key] = $value;
+	}
+}
+
+$output = $gm->run();
 
 return $output;
